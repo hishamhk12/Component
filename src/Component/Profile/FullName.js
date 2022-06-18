419b73fa02e3830d2f1bedb437310308aaa4099c
hishamhk12 <80429993+hishamhk12@users.noreply.github.com>
2022-06-18 11:29:57 +0200

    Added my project

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FullName() {
+  return (
+    <div>HISHAM TAMIM</div>
+  )
+}
---
+++ src/Component/Profile/Profile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Pic from './profil-linkedin-1-300x300.jpg'
+
+export default function Profile() {
+  return (
+    <div><img src={Pic} alt="Logo" />;</div>
+  )
+}
